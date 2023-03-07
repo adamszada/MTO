2019a10defc83be9164adb4e2dfbd22535750149
Adam Szada-Borzyszkowski <103572324+adamszada@users.noreply.github.com>
2023-03-07 18:00:05 +0100

    Add swapCase

--- lab2/main.java
+++ lab2/main.java
@@ -4,13 +4,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class lab2 {
+
+	public static String swapCase(String s){
+		char[] tmp = s.toCharArray();
+		for(int i=0;i<tmp.length;i++){
+			char c = tmp[i];
+			if(Character.isUpperCase(c))
+				tmp[i] = Character.toLowerCase(c);
+			else if(Character.isLowerCase(c))
+				tmp[i] = Character.toUpperCase(c);
+		}
+		return new String(tmp);
+	}
+
 	public static void my_printf(String format_string, String param){
 		Pattern pattern = Pattern.compile("#(\\.\\d+)?k");
 		Matcher matcher = pattern.matcher(format_string);
-		if(!matcher.find())
+		if(!matcher.find()){
+			System.out.println(format_string);
 			return;
+		}
 		String tmp_str = format_string.substring(matcher.start(), matcher.end());
 		int len = matcher.group(1) == null ? param.length() : Math.min(Integer.parseInt(matcher.group(1).substring(1)), param.length());
+		param = swapCase(param);
 		System.out.println(format_string.replace(tmp_str, param.substring(0, len)));
 		System.out.println("");
 	}
