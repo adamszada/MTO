65de515a5d7d02a77a3b238604572e41f0767d02
Adam Szada-Borzyszkowski <103572324+adamszada@users.noreply.github.com>
2023-03-07 17:54:59 +0100

    First change

--- lab2/main.java
+++ lab2/main.java
@@ -1,18 +1,21 @@
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-class lab0 {
+class lab2 {
 	public static void my_printf(String format_string, String param){
-		for(int i=0;i<format_string.length();i++){
-			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
-				System.out.print(param);
-				i++;
-			}else{
-				System.out.print(format_string.charAt(i));
-			}
-		}
+		Pattern pattern = Pattern.compile("#(\\.\\d+)?k");
+		Matcher matcher = pattern.matcher(format_string);
+		if(!matcher.find())
+			return;
+		String tmp_str = format_string.substring(matcher.start(), matcher.end());
+		int len = matcher.group(1) == null ? param.length() : Math.min(Integer.parseInt(matcher.group(1).substring(1)), param.length());
+		System.out.println(format_string.replace(tmp_str, param.substring(0, len)));
 		System.out.println("");
 	}
+
+
 	public static void main(String[] args) throws IOException {
 		//System.out.println("Hello, World!"); 
 		BufferedReader bufferReader=new BufferedReader(new InputStreamReader(System.in));
