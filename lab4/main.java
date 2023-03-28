adc75408838f64fe4c7c09ce667e764bb9fb9e98
Adam Szada-Borzyszkowski <103572324+adamszada@users.noreply.github.com>
2023-03-28 17:18:56 +0200

    Clean main.java

--- lab4/main.java
+++ lab4/main.java
@@ -1,18 +1,12 @@
 
 import java.io.*;
 
-class lab0 {
+class lab4 {
 	public static void my_printf(String format_string, String param){
-		for(int i=0;i<format_string.length();i++){
-			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
-				System.out.print(param);
-				i++;
-			}else{
-				System.out.print(format_string.charAt(i));
-			}
-		}
-		System.out.println("");
+		System.out.print(format_string);
 	}
+
+	
 	public static void main(String[] args) throws IOException {
 		//System.out.println("Hello, World!"); 
 		BufferedReader bufferReader=new BufferedReader(new InputStreamReader(System.in));
