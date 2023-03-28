8477d51af043b8bb7579c0585157071d158845fb
Adam Szada-Borzyszkowski <103572324+adamszada@users.noreply.github.com>
2023-03-28 17:32:40 +0200

    Add reverse number function

--- lab4/main.java
+++ lab4/main.java
@@ -3,6 +3,21 @@ import java.io.*;
 import java.util.regex.*;
 
 class lab4 {
+
+	public static String handleNumber(int number){
+		boolean isNegative = number<0;
+		number = Math.abs(number);
+		int reverse = 0;
+		while(number != 0) {
+			int remainder = number % 10;
+			reverse = reverse * 10 + remainder;
+			number = number/10;
+		}
+		if(isNegative)
+			reverse*=-1;
+		return Integer.toString(reverse);
+	}
+
 	public static void my_printf(String format_string, String param){
 		Pattern pattern = Pattern.compile("#g");
 		Matcher matcher = pattern.matcher(format_string);
@@ -18,6 +33,7 @@ class lab4 {
 			System.out.println(format_string);
 			return;
 		}
+		param = handleNumber(tmpNumber);
 		System.out.println(format_string.replaceAll(replacement, param));
 	}
 
