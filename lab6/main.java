51be5109b91a835a75bf20d41dace3ec95c01c09
Adam Szada-Borzyszkowski <103572324+adamszada@users.noreply.github.com>
2023-04-18 17:33:23 +0200

    Rename

--- lab6/main.java
+++ lab6/main.java
@@ -4,44 +4,45 @@ import java.util.regex.*;
 
 class lab6 {
 	public static void my_printf(String format_string, String param){
-		Pattern pattern = Pattern.compile("#\\.(\\d+)g");
-		Matcher matcher = pattern.matcher(formatString);
+        Pattern pattern = Pattern.compile("#\\.(\\d+)g");
+        Matcher matcher = pattern.matcher(format_string);
 
-		if (!matcher.find()) {
-			System.out.println(formatString);
-			return;
-		}
-		
-		String number = matcher.group(1);
-		int num = 0;
-		
-		try {
-			num = Integer.parseInt(number);
-		} catch (NumberFormatException e) {
-			System.out.println(formatString);
-			return;
-		}
+        if (!matcher.find()) {
+            System.out.println(format_string);
+            return;
+        }
 
-		int val = 0;
+        String replacement = matcher.group(1);
+        int tmpNumber  = 0;
 
-		try {
-			val = Integer.parseInt(param);
-		} catch (NumberFormatException e) {
-			System.out.println(formatString);
-			return;
-		}
+        try {
+            tmpNumber  = Integer.parseInt(replacement );
+        } catch (NumberFormatException e) {
+            System.out.println(format_string);
+            return;
+        }
 
-		String replaceWith = String.format("%0" + num + "d", Math.abs(val))
-				.chars()
-				.map(Character::getNumericValue)
-				.map(c -> (c * 9 + 1) % 10)
-				.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
-				.toString();
+        int val = 0;
 
-		if (val < 0)
-			replaceWith = "-" + replaceWith;
+        try {
+            val = Integer.parseInt(param);
+        } catch (NumberFormatException e) {
+            System.out.println(format_string);
+            return;
+        }
 
-		System.out.println(formatString.replace(matcher.group(0), replaceWith));
+        replacement = String.format("%0" + tmpNumber  + "d", Math.abs(val))
+            .chars()
+            .map(Character::getNumericValue)
+            .map(c -> (c * 9 + 1) % 10)
+            .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+            .toString();
+
+        if (val < 0)
+            replacement = "-" + replacement;
+
+        System.out.println(format_string.replace(matcher.group(0), replacement));
+
 	
 	}
 
