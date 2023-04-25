7f10dfeb5ef9fbc68f7a54f5bc3cdaa3eeed89c9
Adam Szada-Borzyszkowski <103572324+adamszada@users.noreply.github.com>
2023-04-25 17:24:23 +0200

    Create HashMap

--- lab7/main.java
+++ lab7/main.java
@@ -1,7 +1,18 @@
 
 import java.io.*;
+import java.util.HashMap;
 
 class lab7 {
+	
+	private static final HashMap<Character, Character> replacements = new HashMap<>() {{
+		put('a', 'g');
+		put('b', 'h');
+		put('c', 'i');
+		put('d', 'j');
+		put('e', 'k');
+		put('f', 'l');
+	}};
+
 	public static void my_printf(String format_string, String param){
 		
 		System.out.println("");
