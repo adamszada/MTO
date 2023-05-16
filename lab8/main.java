58c21207bfea8813bc2cc7b2270154bed3383849
adamszada <235985@edu.p.lodz.pl>
2023-05-16 17:25:36 +0200

    Clear main.java

--- lab8/main.java
+++ lab8/main.java
@@ -1,16 +1,9 @@
 
 import java.io.*;
 
-class lab0 {
+class lab8 {
 	public static void my_printf(String format_string, String param){
-		for(int i=0;i<format_string.length();i++){
-			if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
-				System.out.print(param);
-				i++;
-			}else{
-				System.out.print(format_string.charAt(i));
-			}
-		}
+		
 		System.out.println("");
 	}
 	public static void main(String[] args) throws IOException {
