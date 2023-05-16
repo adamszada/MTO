b26fad859ec894c5bb1cdaa60cb777043779cde7
adamszada <235985@edu.p.lodz.pl>
2023-05-16 17:27:48 +0200

    Main.java changes

--- lab8/main.java
+++ lab8/main.java
@@ -1,10 +1,39 @@
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class lab8 {
+	private static final HashMap<Character, Character> replacements = new HashMap<>() {{
+		put('a', 'g');
+		put('b', 'h');
+		put('c', 'i');
+		put('d', 'j');
+		put('e', 'k');
+		put('f', 'l');
+	}};
+
 	public static void my_printf(String format_string, String param){
-		
-		System.out.println("");
+		Pattern pattern = Pattern.compile("#j");
+		Matcher matcher = pattern.matcher(format_string);
+		if(!matcher.find()){
+			System.out.println(format_string);
+			return;
+		}
+		long tmpNumber;
+		try{
+			tmpNumber = Long.parseLong(param);
+		} catch (NumberFormatException e) {
+			System.out.println(format_string);
+			return;
+		}
+		String replacement = Long.toHexString(tmpNumber);
+		for (char k : replacements.keySet()) {
+			replacement = replacement.replace(k, replacements.get(k));
+		}
+
+		System.out.println(format_string.replace(matcher.group(0), replacement));
 	}
 	public static void main(String[] args) throws IOException {
 		//System.out.println("Hello, World!"); 
